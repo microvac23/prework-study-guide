bfa2c43d5b3883a61b22b09b9d4982a3b42e240f
microvac23 <Ithanmachuca@gmail.com>
2023-03-26 17:43:26 -0500

    added JavaScript

---
+++ prework-study-guide/assets/script.js
@@ -0,0 +1,16 @@
+var topic=["HTML", "CSS", "Git", "JavaScript"];
+for (var x=0; x < topic.length; x++){
+    console.log(topic[x]);
+} 
+
+/* if (topic === 'HTML') {
+    console.log("Let's Study HTML!");
+}else if (topic === 'CSS') {
+    console.log ("Let's Study CSS!");
+}else if (topic === 'Git') {
+    console.log("Let's Study Git!");
+}else if (topic === 'JavaScript'){
+    console.log("Let's Study JavaScript!");
+}else {
+    console.log('Please try again!');
+} */
